f70abda4d973f359890a9195bcd65f90584caaa4
benjaminrall <33876244+benjaminrall@users.noreply.github.com>
2021-02-26 21:31:01 +0000

    capsule

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float maxSpeed;
+    
+    public float acceleration;
+
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
