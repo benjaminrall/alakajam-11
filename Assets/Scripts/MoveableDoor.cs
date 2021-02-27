258ac6c1af6c69aa229812e13e431b171230fb8d
benjaminrall <33876244+benjaminrall@users.noreply.github.com>
2021-02-27 19:20:43 +0000

    Door

--- Assets/Scripts/MoveableDoor.cs
+++ Assets/Scripts/MoveableDoor.cs
@@ -4,7 +4,28 @@ using UnityEngine;
 
 public class MoveableDoor : InteractableObject
 {
-    public Vector3 activateOffset;
+    public int doorState = 0;
 
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
+    public override void ActivateToggle()
+    {
+        if (doorState <= 0)
+        {
+            doorState = 1;
+        }
+        else
+        {
+            doorState = -1;
+        }
+    }
+
+    private void Update()
+    {
+        animator.SetInteger("DoorState", doorState);
+    }
 }
