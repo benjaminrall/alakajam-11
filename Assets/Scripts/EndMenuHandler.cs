c217c5bf56f14ffc731261cf1ae6539ec9b53737
Maria Brabham <mattbrabham@hotmail.com>
2021-02-28 18:25:20 +0000

    End screen

---
+++ Assets/Scripts/EndMenuHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndMenuHandler : MonoBehaviour
+{
+
+    public Text timerText;
+
+    public void Awake()
+    {
+        timerText.text = GameObject.FindObjectOfType<TimerScript>().FinalTime;
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public int Milliseconds;
+    public int Seconds;
+    public int Minutes;
+
+    public bool TimerActive = true;
+
+    public string FinalTime;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        TimerActive = true;
+    }
+    void FixedUpdate()
+    {
+        if (TimerActive == true)
+        {
+            Milliseconds -= 1;
+            if (Milliseconds <= 0)
+            {
+                Seconds -= 1;
+                Milliseconds = 59;
+            }
+            if (Seconds <= 0)
+            {
+                Minutes -= 1;
+                Seconds = 59;
+            }
+        }
+    }
+
+    public void StopTimer()
+    {
+        TimerActive = false;
+        if (Seconds < 10)
+        {
+            FinalTime = Minutes.ToString() + ":0" + Seconds.ToString();
+        }
+        else
+        {
+            FinalTime = Minutes.ToString() + ":" + Seconds.ToString();
+        }
+    }
+}
