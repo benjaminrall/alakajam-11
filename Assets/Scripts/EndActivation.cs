6dbf83fafeb4e6419ea134d4c183cd9f2b2fcdbb
benjaminrall <33876244+benjaminrall@users.noreply.github.com>
2021-02-28 17:53:36 +0000

    stuff

---
+++ Assets/Scripts/EndActivation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndActivation : InteractableObject
+{
+    public GameManager gameManager;    
+
+    public override void ActivateToggle()
+    {
+        StartCoroutine(LoadEndScene());
+    }
+
+    private IEnumerator LoadEndScene()
+    {
+        gameManager.FadeIn(1.0f);
+
+        yield return new WaitForSeconds(1.0f);
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(2);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
