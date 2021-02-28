02a65fb9ba68a24b7ad73c98d76d87cdf8f9b426
benjaminrall <33876244+benjaminrall@users.noreply.github.com>
2021-02-28 16:43:49 +0000

    yes

---
+++ Assets/Scripts/CrateRope.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateRope : InteractableObject
+{
+    public override void ActivateToggle()
+    {
+        if (!active) active = true;
+        else gameObject.SetActive(false);
+    }
+}
